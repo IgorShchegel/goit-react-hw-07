cd48a34d40e5af90406b177d2db286e4391cfe97
IgorShchegel <142222252+IgorShchegel@users.noreply.github.com>
2024-05-24 23:16:16 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from "react-redux";
+import Contact from "../Contact/Contact"
+import css from './ContactList.module.css'
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) => {
+    return contact.name.toLowerCase().includes(filter.toLowerCase());
+  });
+
+
+  return (
+    <ul className={css.list}>
+      {filteredContacts.map((contact) => (
+        <li  key={contact.id} >
+          <Contact  {...contact} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default ContactList
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const contactsInitialState = {
+  items: [],
+};
+
+const contactsSlice = createSlice({
+  name: "contacts",
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.items.push(action.payload);
+      },
+      prepare(value) {
+        return {
+          payload: {
+            ...value,
+            id: nanoid(),
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      state.items = state.items.filter(
+        (contact) => contact.id !== action.payload
+      );
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
+export const selectContacts = (state) => state.contacts.items;
